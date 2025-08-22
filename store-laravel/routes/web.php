a498bb6ed421857b91dda4e9e477952b97f4bac3
GiorgioHerson <giorgioherson@gmail.com>
2025-08-22 18:15:56 +0700

    15

--- store-laravel/routes/web.php
+++ store-laravel/routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\storeController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ProductCategoryController;
 
 Route::get('/', [storeController::class, 'index'])
 
@@ -25,3 +25,15 @@ Route::get('/product-details', [ProductController::class, 'show'])
     Route::get('/catalog', [ProductController::class, 'index'])
 
     ->name('catalog.index');
+
+    Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
